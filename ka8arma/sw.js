1a54740f0457546722de492cf556c2641a5f6a17
frognard <89304472+frognard@users.noreply.github.com>
2026-06-23 22:27:33 +0300

    Update sw.js

--- ka8arma/sw.js
+++ ka8arma/sw.js
@@ -1,23 +1,22 @@
-const CACHE_NAME = "ka8arma-v1";
+const CACHE_NAME = 'ka8arma-v1';
 
-const urlsToCache = [
-    "./",
-    "./ka8arma.html",
-    "./manifest.json",
-    "./icon-192.png",
-    "./icon-512.png"
-];
-
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        './',
+        './ka8arma.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
 });
 
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-    );
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
